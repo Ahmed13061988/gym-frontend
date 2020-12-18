baba84f1f8d253a56debfee526869c88c9e9f2cf
Ahmed <wolktheline@yahoo.com>
2020-12-18 12:02:08 -0700

    adding the userReducers to the combine reducers

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,6 +1,7 @@
 import gymsReducer from './gymsReducer'
 import {combineReducers} from 'redux'
 import userReducer from './userReducer'
+import gymsReducer from './gymsReducer'
 
 
 export const reducer = combineReducers({gyms: gymsReducer , user: userReducer})
