6f21836ebb5b293241eb64db5279b6a58cc0cd4c
Ahmed <wolktheline@yahoo.com>
2020-12-18 14:01:42 -0700

    Adding the Login form with username and password

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,10 +3,36 @@ import { connect } from "react-redux";
 
 const Login = (props) => {
   
-    return (
-        <h1>Login</h1>
-    )
+    const { signup, toggleSignup } = props
+  return(
+    <>
+      <h3>{signup ? "Sign up!" : "Login!"} </h3>
+      <form>
+        <label>
+        Username:
+          <input type="text" name="username" />
+        </label><br/>
+        <label>
+          Password:
+          <input type="password" name="password" />
+        </label><br/>
+        {signup &&
+          <>
+            <label>
+              Password Confirmation:
+         <input type="password" name="passwordConfirmation" />
+            </label>
+            <input type="submit" value="Submit" />
+          </>
+        }
+      </form>
+      <br/>
+      <br/>
+      <button onClick={toggleSignup}>Or... {signup ? "Login!" : "Sign up!"}</button>
+    </>
+  )
 }
+
 
 
 
