aea7df9b2f7e382e7f80c61b9b90e9c27502ef70
Ahmed <wolktheline@yahoo.com>
2020-12-17 20:31:09 -0700

    adding the containers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 
 import './App.css';
 import ReactDom from "react-dom"
+import GymCards from './containers/GymCards'
 
 function App() {
   return (
+    <>
+   
     <div className="App">
     <h1>Find your workout place</h1>
+    < GymCards />
     </div>
+    </>
   );
 }
 
---
+++ src/containers/GymCards.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GymCards = () =>{
+
+    return(
+        <div className="cards">
+            cards 
+        </div>
+    )
+}
+
+export default GymCards;
